123cd9c99d47c1a914a74042d3b24cdeca97f4ff
Harsh <harshshinde5606@gmail.com>
2022-05-21 01:31:00 +0530

    ready to add functionality to form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -5,14 +5,15 @@ const ContactForm = () => {
   return (
     <>
       <div className="contact-container" data-aos="zoom-in">
-        <form action="">
+        <form action="" onSubmit="">
           <h3>Fill the Following</h3>
-          <input id="name-input" type="text" placeholder="Enter your name" />
-          <input id="email-input" type="text" placeholder="Enter your email" />
+          <input id="name-input" type="text" placeholder="Enter your name" required/>
+          <input id="email-input" type="text" placeholder="Enter your email" required/>
           <textarea
             name="email"
             id="message-input"
             placeholder="Enter a short message"
+            required
           ></textarea>
           <button className="submitBtn">Submit</button>
         </form>
