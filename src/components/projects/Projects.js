36703550c7a2f09eca814c03fa7b64fe3c83a43e
Harsh shinde <harshshinde5606@gmail.com>
2022-06-09 08:47:48 +0530

    added  upbox instead of loop-studio

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,8 +12,8 @@ const Projects = () => {
           <button id="caseStudy"><a target={"_blank"} href="https://space-tourists.web.app/" style={{ color: "black", textDecoration: "none"}}> Case Study </a></button> 
         </div>
         <div className="project-2" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
-          <h2 id="title">Loop-Studio</h2>
-          <button id="caseStudy">Case Study</button>
+          <h2 id="title">Upbox</h2>
+          <button id="caseStudy"><a target={"_blank"} href="https://harsh7800.github.io/Upbox/" style={{ color: "black", textDecoration: "none"}}> Case Study </a></button>
         </div>
         <div className="project-3" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
           <h2 id="title">Snap</h2>
