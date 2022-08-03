02a939ae6d019fefbe8f35dab68d36f88190b8a5
Harsh shinde <harsh@sixpep.com>
2022-08-03 13:22:59 +0530

    resolved Link tag error

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -9,15 +9,15 @@ const Projects = () => {
       <div className="projects-container">
         <div className="project-1" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
           <h2 id="title">Space-Tourist</h2>
-          <button id="caseStudy"><Link target={"_blank"} href="https://space-tourists.web.app/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button> 
+          <button id="caseStudy"><Link target={"_blank"} to="https://space-tourists.web.app/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button> 
         </div>
         <div className="project-2" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
           <h2 id="title">Upbox</h2>
-          <button id="caseStudy"><Link target={"_blank"} href="https://harsh7800.github.io/Upbox/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button>
+          <button id="caseStudy"><Link target={"_blank"} to="https://harsh7800.github.io/Upbox/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button>
         </div>
         <div className="project-3" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
           <h2 id="title">Snap</h2>
-          <button id="caseStudy"><Link target={"_blank"} href="https://harsh7800.github.io/Snap-Reactjs/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button>
+          <button id="caseStudy"><Link target={"_blank"} to="https://harsh7800.github.io/Snap-Reactjs/" style={{ color: "black", textDecoration: "none"}}> Case Study </Link></button>
         </div>
       </div>
     </>
